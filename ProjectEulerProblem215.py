6bdef1ca48d14775b2a4ad0d54f58866dbc0c9c1
Ruxton07 <rt.kellar@gmail.com>
2024-01-29 13:41:47 -0600

    Started problem 215

--- ProjectEulerProblem215.py
+++ ProjectEulerProblem215.py
@@ -2,7 +2,15 @@ from math import factorial
 from time import sleep
 
 def W(x, y):
+    crackLocations = []
+
+def computeCracks():
     print("")
 
+def main():
+    ROW_LEN = 9
+    COL_LEN = 3
+    print(W(ROW_LEN, COL_LEN))
+    
 if __name__ == "__main__":
-    print(W(9, 3))
+    main()
