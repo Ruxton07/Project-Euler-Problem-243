1d5c4b2e3a2b847669bcd8a9dd0b2412c89c89cc
Ruxton07 <rt.kellar@gmail.com>
2024-02-25 22:44:22 -0600

    Implemented 215 solution

--- ProjectEulerProblem215.py
+++ ProjectEulerProblem215.py
@@ -3,13 +3,27 @@ from time import sleep
 
 def W(x, y):
     crackLocations = []
-
-def computeCracks():
-    print("")
+    def getCracks(c, p):
+        if p < x:
+            for i in (1, 2):
+                c.append(p+i+1)
+                getCracks(c, p+i+1)
+                c.pop()
+        elif p == x:
+            crackLocations.append(set(c[:-1]))
+        else:
+            return
+    getCracks([], 0)
+    ncindex = [[i for (i, crackpos2) in enumerate(crackLocations) if crackpos1.isdisjoint(crackpos2)] for crackpos1 in crackLocations]
+    combs = [1] * len(crackLocations)
+    for i in range(1, y):
+        newcombs = [sum(combs[k] for k in nci) for nci in ncindex]
+        combs = newcombs
+    return str(sum(combs))
 
 def main():
-    ROW_LEN = 9
-    COL_LEN = 3
+    ROW_LEN = 32
+    COL_LEN = 10
     print(W(ROW_LEN, COL_LEN))
     
 if __name__ == "__main__":
